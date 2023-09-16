2008818bfd977571e086b8675064cd58cd62a32b
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-09-16 13:21:52 -0600

    Agregado código para funciones nuevas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 //Aquí se inicia el servidor 
 const express = require('express'); //requerimos el modulo y lo guardamos en una constante del mismo nobre
+const morgan = require('morgan');
 const app = express(); //lo voy a ejecutar y lo voy a almacenar en un objeto  app se convierte en nuestro servidor
 
+
+
 //Settings
-//
-//Middlewares
+app.set('port', process.env.PORT || 3000) //esto es para decirle que tome el puerto del sistema operativo, en servicios de la nuebe la herramienta te da el puerto
+//Middlewares = son funciones que se ejecutana antes de que lleguen a nuestras rutas.
+app.use(morgan('dev')   );
+app.use(express.json()) // devuelve los datos de un archivo json 
 
 //Routes
 
 //Static files
 
 //Starting the server
-app.listen(3000,()=>{ //Con esto hacemos qe escuche el puerto 3000 y que se ejecute alguna tarea
- console.log("Server on port 3000") //En este caso la orden  en este caso que avise que el servidor esta en el puerto 3000
-})
+app.listen(app.get('port'),()=>{ //Con esto hacemos qe escuche el puerto 3000 y que se ejecute alguna tarea// estamos obteniednod el valor de puerto
+ console.log( `Server on port ${app.get('port')}`) //En este caso la orden  en este caso que avise que el servidor esta en el puerto 3000
+});
